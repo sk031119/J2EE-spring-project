8be4774054b9155d438d90717d5d05b94fc8cc38
samuel law <samuel.law_hk@yahoo.com.hk>
2026-04-15 17:50:20 -0400

    init

---
+++ quick job system/src/main/java/com/samuellaw/quick_job_system/QuickJobSystemApplication.java
@@ -0,0 +1,13 @@
+package com.samuellaw.quick_job_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickJobSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickJobSystemApplication.class, args);
+    }
+
+}
---
+++ quick job system/src/test/java/com/samuellaw/quick_job_system/QuickJobSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuellaw.quick_job_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickJobSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
